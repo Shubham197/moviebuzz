39935dc91bd96b8f37774e4688364022d16e0274
Shubham197 <35462199+Shubham197@users.noreply.github.com>
2018-08-05 23:14:40 +0530

    adding comment

--- lib/package.py
+++ lib/package.py
@@ -1,6 +1,7 @@
-# import urllib.request
-# import ssl
-# import re
+import urllib.request
+#import ssl
+#import re
+#import os
 import urllib.request as req
 from bs4 import BeautifulSoup
 from subprocess import call # module to create subprogram inside our program
--- lib/video.py
+++ lib/video.py
@@ -1,25 +1,32 @@
-# from past.builtins import execfile
-# execfile('package.py')
+#from past.builtins import execfile
+#execfile('package.py')
+import urlib.request
+from bs4 import Beautifulsoup
 
-query=input("Enter The Video Name :\n").replace(" ","%20")
-res= req.urlopen("https://www.youtube.com/results?search_query="+query)
-data = res.read().decode("utf-8")
-soup = BeautifulSoup(data,"html.parser")
+def get_movie_choice(video_name):
+    query = video_name.replace("","%20")
+    res= req.urlopen("https://www.youtube.com/results?search_query="+query)
+    data = res.read().decode("utf-8")
+    soup = BeautifulSoup(data,"html.parser")
+    hyper = soup.find_all("a",class_="yt-uix-tile-link")
 
-hyper = soup.find_all("a",class_="yt-uix-tile-link")
-links = []
-
-for index,i in enumerate(hyper):
-    print(index+1,i.get("title"))
-    links.append(i.get("href"))
+    links = []
+    for index,i in enumerate(hyper):
+        print(index+1,i.get("title"))
+        links.append(i.get("href"))
     
-choice=int(input("\nEnter Your Choice :\n"))
-videourl = "https://www.youtube.com"+links[choice-1]
+    choice=int(input("\nEnter Your Choice :\n"))
+    videourl = "https://www.youtube.com"+links[choice-1] ####video url open
+    return videourl
 
-call([r"C:\Program Files\VideoLAN\VLC\vlc.exe",videourl])
+def start_downloading(video_url):
+    call([r"C:\Program Files\VideoLAN\VLC\vlc.exe",videourl])
+    call("youtube-dl "+videourl)
 
-print("Started Download")
-call("youtube-dl "+videourl)
-                
-print("End Download\n")
-input()
+def download_video():
+    video_name=input("\t Please provide YouTube video name: ")
+    video_url = get_movie_choice(video_name)
+    print("\nStart download\n")
+    start_downloading(video_url)
+    print("End download")
+#input()
